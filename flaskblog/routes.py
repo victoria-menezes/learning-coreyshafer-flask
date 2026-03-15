7c9f1dafe720de7f1d8a3a1f38f70a070ab17cb0
victoria-menezes <victoria.c.menezes@gmail.com>
2026-03-14 21:03:30 -0300

    Implementing delete post (UNFINISHED)

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect, url_for, request
+from flask import render_template, flash, redirect, url_for, request, abort
 from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from flaskblog.models import User, Post
@@ -205,4 +205,67 @@ def new_post():
         'post_new.html',
         title='New Post',
         form = form
+    )
+
+# specific page per post
+@app.route(
+    '/post/<int:post_id>'
+)
+def post(
+    post_id : int
+    ):
+    post = Post.query.get_or_404(post_id) # returns 404 if it doesnt exist
+
+    return render_template(
+        'post.html',
+        title=post.title,
+        post = post
+    )
+@app.route(
+    '/post/<int:post_id>/update',
+    methods = ['GET', 'POST']
+)
+@login_required
+def update_post(
+        post_id : int
+    ):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    
+    form = PostForm()
+
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+
+        db.session.commit()
+
+        flash('Post updated.', 'success')
+        return redirect(url_for('post', post_id = post.id))
+    
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+    
+    return render_template(
+        'post_new.html',
+        title = 'Update Post',
+        post = post,
+        form = form,
+        legend='Update Post'
+    )
+
+    
+@app.route(
+    '/post/<int:post_id>/delete',
+    methods = ['POST']
+)
+@login_required
+def delete_post(post_id : int):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    
+    return render_template(
     )
